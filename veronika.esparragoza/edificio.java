9b41a76b5b12f4075d2cef10a066fd3b081c5820
VeronikaEspa <vesparragozam@gmail.com>
2022-04-26 11:05:08 +0200

    Avanzando en el proyecto

--- veronika.esparragoza/edificio.java
+++ veronika.esparragoza/edificio.java
@@ -1,14 +1,36 @@
 public class edificio{
+    
+    
     public static void main(String[] args){
-    for (int i=1; i<=7;i= i+1){
-        for (int j=1;j<=5;j=j+1){
-            if(i == 4){
-                System.out.print(" [0] ");
-            } else {
-                System.out.print(" [.] ");
+        for (int dia = 1; dia <=3; dia = dia+1){
+            System.out.println("El dia es: " + dia);
+
+            for (int hora = 1; hora <=24; hora = hora+1){
+
+                float ventana = (float) Math.random();
+                float luz = (float) Math.random();
+                
+                System.out.println("La hora es: " + hora + " del dia " + dia);
+
+                for (int i = 7; i >= 1; i = i-1 ){
+                    System.out.print(i);
+                    for (int j = 1; j <=5; j = j+1 ){
+                        if(ventana <= 0.7){
+                            if(luz <= 0.6){
+                                System.out.print(" [o] "); // con luz
+                            } else{
+                                System.out.print(" [.] "); // fuera luz
+                            }
+                        } else {
+                            System.out.print(" [#] "); // cerrada
+                        }
+                }
+                System.out.println();
             }
+
+            System.out.println();
         }
-         System.out.println();
+        System.out.println();
     }
 }
 }
