cc9ff8f87b48f58d95f54cddd2154e29eeba124c
David Montenegro <68624139+Dmon4516@users.noreply.github.com>
2022-04-26 11:09:18 +0200

    1.2

--- David.Montenegro/Codigoex.java
+++ David.Montenegro/Codigoex.java
@@ -1,18 +1,28 @@
 public class Codigoex{
     public static void main (String[] args){
-        int altura = 7;
-        int dia = 0;
-        int horas = 0;
-        while ( dia < 3 ){
-            double pr = Math.random() * 4 + 1;
-            double pap = Math
-            while (horas < 24){
-                for ( int columnas = 1 ; columnas < altura ; columnas = columnas + 1){
-                    for (int pisos = 1 ; pisos < 5 ; pisos = pisos + 1 ){
+        int altura = 8;
+        int dia = 1;
+        int horas = 1;
+        while ( dia < 4 ){
+            double pr = Math.random();
+            double pap = Math.random();
+            System.out.println("Dia : " + dia );
+            int averiaR = 0 ;
+            int averiaP = 0 ;
+            int columnas = 1 ;
+            int pisos = 1 ;
+            while (horas < 25){
+                System.out.println("Hora : " + horas );
+                for ( int pisos = 1 ; pisos < altura ; pisos = pisos + 1){
+                    System.out.print( 8 - columnas);
+                    if (pr <= 0.25){
+                        
+                    }
+                    for (columnas <= 5 ; columnas = columnas + 1 ){
                         double pva = Math.random();
                         double ple = Math.random();
                         if (pva <= 0.30){
-                            System.out.print("[#])");
+                            System.out.print("[#]");
                         } else if ( pva > 0.30 && ple <= 0.40){
                             System.out.print("[.]");
                         } else if (pva > 0.30 && ple > 0.40){
@@ -25,6 +35,7 @@ public class Codigoex{
                 System.out.println("---------------------------");
             }
             dia = dia + 1;
+            horas = 1 ;
         }
     }
 }
