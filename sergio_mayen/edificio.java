2cb37f4a0413054ac4d376a4fce4ceead90053a9
Sergio Mayen <sergio.mayen@ct.uneatlantico.es>
2022-04-26 11:45:44 +0200

    No me sale

--- sergio_mayen/edificio.java
+++ sergio_mayen/edificio.java
@@ -8,22 +8,12 @@ public class edificio{
 
         double ventanaAbiertaCerrada, ventanaEncendidaApagada, averia, rayo;
         int dia = 0;
-        int matriz[][] = new int[7][5];	
 
-        while(dia > 5 ){
+        while(dia > 5){
             dia = dia + 1;
 
-            for (int x=0; x < matriz.length; x++) {
-                System.out.print("|");
-                for (int y=0; y < matriz[x].length; y++) {
-                  System.out.print (matriz[x][y]);
-                  if (y!=matriz[x].length-1) System.out.print("\t");
-                }
-                System.out.println("|");
-            }
-
             ventanaAbiertaCerrada = Math.random();
-            if(ventanaAbiertaCerrada <= 0.30){
+            if(ventanaAbiertaCerrada <=0.30){
                 System.out.println("Ventana cerrada" + ventanaAbiertaCerrada);
             }else{
                 System.out.println("Ventana abierta" + ventanaAbiertaCerrada);
@@ -31,28 +21,28 @@ public class edificio{
 
 
             ventanaEncendidaApagada = Math.random();
-            if(ventanaEncendidaApagada <= 0.40){
+            if(ventanaEncendidaApagada <=0.40){
                 System.out.println("Ventana apagada" +ventanaEncendidaApagada);
             }else{
                 System.out.println("Ventana encendida" +ventanaEncendidaApagada);
             }
 
             rayo = Math.random();
-            if(rayo <= 0.25){
+            if(rayo <=0.25){
                 System.out.println("Rayo cayo :(" + rayo);
             }else{
                 System.out.println("No paso nada :)" + rayo);
             }
 
             averia = Math.random();
-            if(rayo <= 0.15){
+            if(rayo <=0.15){
                 System.out.println("Se rompio fila :(" + averia);
             }else{
                 System.out.println("No paso nada :)" + averia);
             }
+
+            System.out.println("");
+            siguiente.nextLine();
         }
-        
-        System.out.println("");
-        siguiente.nextLine();
     }
 }
