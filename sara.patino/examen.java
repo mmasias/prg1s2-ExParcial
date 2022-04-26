f985b5e50655325d60f6e66d45383d6fe81c5ac5
sara-patino <sara.patino@alumnos.uneatlantico.es>
2022-04-26 12:00:52 +0200

    avance

--- sara.patino/examen.java
+++ sara.patino/examen.java
@@ -31,7 +31,8 @@ public class examen{
             luzEncendida=[0]
         }else{
             luz=luzApagada;
-            luzApagada=[.]
+            luzApagada=[.];
+            dtydty
         }
     
 
