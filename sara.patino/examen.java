72fa77a4dbe34c17928fde06f8cfbb1b26db599d
sara-patino <sara.patino@alumnos.uneatlantico.es>
2022-04-26 10:51:00 +0200

    progreso1

---
+++ sara.patino/examen.java
@@ -0,0 +1,30 @@
+public class examen{
+    public static void main(String[] args){
+
+        int ventana, ventanaCerrada, ventanaAbierta;
+        int luz, luzApagada, luzEncendida;
+        int hora;
+
+        ventana=(Math.random()*100-0)+0;
+        if(ventana<=70){
+            ventana=ventanaAbierta;
+        }else{
+            ventana=ventanaCerrada;
+        }
+
+        luz=(Math.random()*100-0)+0;
+        if(luz<=60){
+            luz=luzEncendida;
+        }else{
+            luz=luzApagada;
+        }
+
+        (i==j=ventana);
+        for(int i=1; i<=5;i=i+1){
+            for(int j=1;j<=7;j=j+1)
+            if()
+        }
+
+        int hora=hora+1;
+    }
+}
