4128b9356039a9813246c654dae4e5d4f0e60185
Sergio Mayen <sergio.mayen@ct.uneatlantico.es>
2022-04-26 23:07:59 +0200

    Mejora de codigo con probalidad, pero con error al imprimir

--- sergio_mayen/edificio.java
+++ sergio_mayen/edificio.java
@@ -6,65 +6,71 @@ public class edificio{
 
         Scanner siguiente = new Scanner(System.in);
 
-        int dia = 0, hora =0, alturaEdificio = 7, confirmarRayo = 0, confirmarAveria = 0, fila1, fila2, fila3, col1, col2, col3;
-        double ventanaAbiertaCerrada, ventanaEncendidaApagada, averia, rayo;
+        int dia = 0, hora = 0;
+        double ventanaAbiertaCerrada, ventanaEncendidaApagada, averiaNumero, rayoNumero;
 
-        while(dia < 5){
+        while(dia < 3){
             dia = dia + 1;
+
+            int columnaEstropeada = 0;
+            int plantaEstropeada = 0;
+
+            for (int y = 1; y<=7; y++){ 
+                double probabilidadAveria = Math.random();
+
+                if(probabilidadAveria >= 0.85 && plantaEstropeada == 0){
+                    plantaEstropeada = y;
+                }
+            }
+
+            for (int x = 1; x<=5; x++){ 
+                double probabilidadRayo = Math.random();
+
+                if(probabilidadRayo >= 0.75 && columnaEstropeada == 0){
+                    columnaEstropeada = x;
+                }
+            }
 
             ventanaAbiertaCerrada = Math.random();
             if(ventanaAbiertaCerrada <=0.30){
-                System.out.println("Ventana cerrada" + ventanaAbiertaCerrada);
+                System.out.println("Ventana cerrada " + ventanaAbiertaCerrada);
             }else{
-                System.out.println("Ventana abierta" + ventanaAbiertaCerrada);
+                System.out.println("Ventana abierta " + ventanaAbiertaCerrada); 
             }
 
 
             ventanaEncendidaApagada = Math.random();
             if(ventanaEncendidaApagada <=0.40){
-                System.out.println("Ventana apagada" +ventanaEncendidaApagada);
+                System.out.println("Ventana apagada " +ventanaEncendidaApagada);
             }else{
-                System.out.println("Ventana encendida" +ventanaEncendidaApagada);
+                System.out.println("Ventana encendida " +ventanaEncendidaApagada);
             }
 
-            rayo = Math.random();
-            if(rayo <=0.25){
-                System.out.println("Se rompio columna " + rayo);
-                confirmarRayo ++;
-            }else{
-                System.out.println("No paso nada :)" + rayo);
-            }
-
-            averia = Math.random();
-            if(averia <=0.15){
-                System.out.println("Se rompio fila " + averia);
-                confirmarAveria ++;
-            }else{
-                System.out.println("No paso nada :)" + averia);
-            }
-
-            for(int i = 1; i <= 7; i++){
-                for(int j = 1; j <= 5; j++){
-                    if(confirmarAveria == 1 || confirmarRayo == 1){
-                        averia = (int)(Math.random()*(7));
-                        rayo = (int)(Math.random()*(5));
-                        if(i==rayo || j==averia){
-                            System.out.println("`" + j);
-                            System.out.println("/" + i);
+            for(int h = 0; h < 24; h++){
+                System.out.println("Hora " + h + " del dia " + dia);
+                for(int i = 7; i>0; i--){
+                    System.out.print(i);
+                    for(int j = 1; j<=5; j++){
+                        if(i == plantaEstropeada || j == columnaEstropeada){
+                            System.out.print(" [x] ");
+                        }
+                        else if(ventanaAbiertaCerrada){
+                            if(ventanaEncendidaApagada){
+                                System.out.print(" [O] ");
+                            }
+                            else{
+                                System.out.print(" [.] ");
+                            }
+                        }
+                        else{
+                            System.out.print(" [#] ");
                         }
                     }
-                    else{
-                        System.out.print(" . ");
-                    }
-                }
-                System.out.println();
+                    System.out.println(); 
+                } 
+                System.out.println("");
+                siguiente.nextLine(); 
             }
-
-            averia = 0;
-            confirmarAveria = 0;
-            confirmarRayo = 0;
-            System.out.println("");
-            siguiente.nextLine();
         }
     }
 }
