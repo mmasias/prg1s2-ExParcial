f7e76583054a12e291a6638980b96015725ee675
Jorge85015 <jorge.quinonez@alumnos.uneatlantico.es>
2022-04-26 23:57:20 +0200

    Examen Completado Version Final (Ahora si)

--- Jorge.Quiñonez/ExamenEdificio.java
+++ Jorge.Quiñonez/ExamenEdificio.java
@@ -2,70 +2,130 @@ import java.lang.Math;
 public class ExamenEdificio {
     public static void main(String[] args){
         
-        int Dia, VentanaAbierta, LuzEncendida;
+        int Dia, Horas, Pisos, Columnas;
         double ProbVentanaAbierta, ProbLuz;
         int PisoAveriado, ColumnaAveriada;
         double ProbRayo, ProbPisoAveriado;
 
         Dia = 1;
-        VentanaAbierta = 0;
-        LuzEncendida = 0;
+        Horas = 1;
         PisoAveriado = 0;
         ColumnaAveriada =0;
+        Pisos = 1;
+        Columnas = 0;
 
-        while(Dia<3){
+        while(Dia <=3){
             Dia = Dia + 1;
+            ProbRayo = Math.random()*(100);
+            ProbPisoAveriado = Math.random()*(100);
 
-            for(int i=1;i<=5;i=i+1){
-                ProbRayo = Math.random();
-
-                if(ProbRayo >= 0.75 && ColumnaAveriada == 0){
-                    ColumnaAveriada = i;
-                }
+            if(ProbRayo <= 25){
+                ColumnaAveriada = (int)(Math.random()*(5-1)+1);
             }
-            for(int j=1;j<=7;j=j+1){
-                ProbPisoAveriado = Math.random();
-
-                if(ProbPisoAveriado >= 0.85 && PisoAveriado == 0){
-                    PisoAveriado = j;
-                }
+            if(ProbPisoAveriado <= 15){
+               PisoAveriado = (int)(Math.random()*(7-1)+1); 
             }
-
-            for(int horas=0;horas<=24;horas=horas+1){
-                System.out.println();
-                System.out.println("Día: " + Dia + " Hora: " + horas);
-
-                for(int filas=7;filas>0;filas=filas-1){
-                    System.out.print(filas);
-
-                    for(int columnas=1;columnas<=5;columnas=columnas+1){
-                        ProbVentanaAbierta = Math.random();
-                        ProbLuz = Math.random();
-                        if((int)ProbVentanaAbierta < 0.7){
-                            VentanaAbierta = 1;
-                        }
-
-                        if((int)ProbLuz < 0.6){
-                            LuzEncendida = 1;
-                        }
-
-                        if(filas == PisoAveriado || columnas == ColumnaAveriada){
+            while(Horas < 25){
+                Horas = Horas + 1;
+                System.out.println("=============================================");
+                System.out.println("Hora: " + Horas);
+                while(Pisos <=7){
+                    System.out.print(8 - Pisos);
+                    if(PisoAveriado == Pisos){
+                        while(Columnas<6){
                             System.out.print(" [T] ");
-                        }
-                        else if(VentanaAbierta==1){
-                            if(LuzEncendida==1){
-                                System.out.print(" [0] ");
-                            }else{
-                                System.out.print(" [.] ");
-                            }
-                        }
-                        else{
-                            System.out.print(" [#] " );
+                            Columnas = Columnas + 1;
                         }
                     }
-                System.out.println();
+                    else{
+                        while(Columnas <=5){
+                            ProbVentanaAbierta = Math.random()*(100);
+                            ProbLuz = Math.random()*(100);
+
+                            if(ColumnaAveriada == Columnas){
+                                System.out.print(" [T] ");
+                            }
+                            else if(ProbVentanaAbierta <= 30){
+                                System.out.print(" [#] ");
+                            }
+                            else if(ProbVentanaAbierta > 30 && ProbLuz > 40){
+                                System.out.print(" [0] "); 
+                            }
+                            else if(ProbVentanaAbierta > 30 && ProbLuz <= 40){
+                                System.out.print(" [.] "); 
+                            }
+                            Columnas = Columnas + 1;
+                        }
+                    }
+                    Columnas = 1;
+                    Pisos = Pisos + 1;
+                    System.out.println();
                 }
+                Pisos = 1;
             }
+
         }
     }
 }
+
+// while(Dia<=3){
+//     Dia = Dia + 1;
+
+//     for(int i=1;i<=5;i=i+1){
+//         ProbRayo = Math.random();
+
+//         if(ProbRayo >= 0.75 && ColumnaAveriada == 0){
+//             ColumnaAveriada = i;
+//         }
+//     }
+//     for(int j=1;j<=7;j=j+1){
+//         ProbPisoAveriado = Math.random();
+
+//         if(ProbPisoAveriado >= 0.85 && PisoAveriado == 0){
+//             PisoAveriado = j;
+//         }
+//     }
+
+//     for(int horas=0;horas<=24;horas=horas+1){
+//         System.out.println();
+//         System.out.println("Día: " + Dia + " Hora: " + horas);
+
+//         for(int filas=7;filas>0;filas=filas-1){
+//             System.out.print(filas);
+
+//             for(int columnas=1;columnas<=5;columnas=columnas+1){
+//                 ProbVentanaAbierta = (int)(Math.random()*(100));
+//                 ProbLuz = (int)(Math.random()*(100));
+//                 if(ProbVentanaAbierta < 0.7){
+//                     VentanaAbierta = 1;
+//                 }
+//                 else{
+//                     VentanaCerrada = 1;
+//                 }
+
+//                 if(ProbLuz > 40){
+//                     LuzEncendida = 1;
+//                 }
+//                 else{
+//                     LuzApagada = 1;
+//                 }
+
+//                 if(filas == PisoAveriado || columnas == ColumnaAveriada){
+//                     System.out.print(" [T] ");
+//                 }
+//                 else if(VentanaAbierta==1){
+//                     if(LuzEncendida==1){
+//                         System.out.print(" [0] ");
+//                     }
+//                     else if(LuzApagada == 1){
+//                         System.out.print(" [.] ");
+//                     }
+//                 }
+//                 else if(VentanaCerrada == 1){
+//                     System.out.print(" [#] " );
+//                 }
+//             }
+//         System.out.println();
+//         }
+//     }
+// }
