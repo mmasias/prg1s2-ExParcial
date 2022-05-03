724269280eda62bbcf673b8c3726e77dae94aa84
veronika <vesparragozam@gmail.com>
2022-05-03 09:34:21 +0200

    Proyecto finalizado

--- veronika.esparragoza/edificio.java
+++ veronika.esparragoza/edificio.java
@@ -1,37 +1,54 @@
 import java.util.zip.ZipEntry;
 public class edificio{
     public static void main(String[] args){
-
         boolean averiaRayo;
+        boolean averiaPiso;
+		int filaMuerta;
 		int columnaMuerta;
 		for(int days=1; days<=3; days++) {
-			averiaRayo=false;
-			columnaMuerta=0;
+			averiaRayo = false;
+            averiaPiso = false;
+			columnaMuerta = 0;
+            filaMuerta = 0;
+            System.out.println("Comienza un nuevo dia: " + days);
+            System.out.println();
+            System.out.println("-------------------------------");
 			for(int hours=0; hours<=23; hours++) {
-				if (Math.random()<.5 && !caeRayo) {
+				if (Math.random() < 0.25 && !averiaRayo) {
 					averiaRayo=true;
 					columnaMuerta = (int) (Math.random()*5+1);
 					}
-				System.out.println("DIA "+days + " HORA "+hours);
+                if (Math.random() < 0.15 && !averiaPiso) {
+                    averiaPiso = true;
+                    filaMuerta = (int) (Math.random()*7+1);
+                    }
+                System.out.println();
+				System.out.println("Hora: "+hours + " del dia: "+days);
 				for (int pisos=1; pisos<=7; pisos++){
 					for (int flat=1; flat<=5; flat++) {
-						if (averiaRayo && flat==columnaMuerta) {
+						if (averiaRayo && flat == columnaMuerta) {
 							System.out.print(" [T] ");
 						} else {
-							if (Math.random()<=0.7) {
-								if (Math.random()<=0.6) {
-									System.out.print(" [O] ");
-								} else {
-									System.out.print(" [.] ");
-								}
-							} else {
-								System.out.print(" [#] ");	
-							}							
+                            if(averiaPiso && pisos==filaMuerta){
+                                System.out.print(" [P] ");
+                            } else{
+                                if (Math.random()<=0.7) {
+                                    if (Math.random()<=0.6) {
+                                        System.out.print(" [O] ");
+                                    } else {
+                                        System.out.print(" [.] ");
+                                    }
+                                } else {
+                                    System.out.print(" [#] ");
+                                }
+                            }
 						}
 					}
-					System.out.println("");
+					System.out.println();
 				}
 			}
-		}
+            System.out.println("-------------------------------");
+            System.out.println();
+        }
     }
 }
