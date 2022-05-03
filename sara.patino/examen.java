d480c9426286a8b10298866400a22896f7bb3eb2
sara-patino <sara.patino@alumnos.uneatlantico.es>
2022-05-03 09:53:29 +0200

    falta acomodar las columnas y ya

--- sara.patino/examen.java
+++ sara.patino/examen.java
@@ -3,76 +3,38 @@ import java.lang.Math;
 class examen{
     public static void main(String[] args) {
 
-        int dia, hora, ubicacionAveria;
-        int  probabilidadVentana, probabilidadLuz, probabilidadAveria, probabilidadRayo;
-        boolean ventanaAbierta, luzEncendida, caerRayo;
-        String mensaje = " ";
-        ubicacionAveria=0;
-        caerRayo=false;
-        dia = 1;
-        while(dia <= 3 ) {
+        int columnaMuerta, dias, horas, losPisos, departamentos;
+        boolean rayo;
 
-            // //Averia
-            // probabilidadAveria = (Math.random()*(100-0))+0;
+        for(dias=1; dias<=3; dias++){
+            columnaMuerta=0;
+            rayo = false;
+                for(horas=0; horas<=23; horas++){
+                    if((Math.random()*100)<=25 && !rayo){
+                        rayo=true;
+                        columnaMuerta= (int)(Math.random()*5+1);
+                        System.out.println("Cae un rayo");
+                    }
 
-            // if(probabilidadAveria<=15){
-            //     ubicacionAveria=(int)(Math.random()*7+1);
-
-            // }
-
-            // //CaidaRayo
-            // int columnaRayo;
-
-            // columnaRayo= (Math.random()*(100-0))+0;
-            probabilidadVentana = (int)(Math.random()*100);
-            probabilidadLuz = (int)(Math.random()*100);
-            probabilidadRayo = (int)(Math.random()*100);
-
-            for(hora = 1; hora <= 24; hora++){
-
-                System.out.println("Hora " + hora + " del dia " + dia + ": "+mensaje);
-
-
-                if(probabilidadVentana <= 70){
-                    ventanaAbierta=true;
-                }
-                if(probabilidadLuz <= 60){
-                    luzEncendida=true;
-                }
-                if(probabilidadRayo <= 70 && !caerRayo){
-                    caerRayo=true;
-                    ubicacionAveria = (int)(Math.random()*5+1);
-                    mensaje = "Cae un rayo";
-                }
-
-               
-                for(int i = 0; i<=7; i++){
-                    System.out.print(i);
-                    for(int j = 1; j<=5; j++){
-    
-                        if(caerRayo=true && j==ubicacionAveria){
-                            System.out.print(" [R] ");
-                        } else {
-                        if(ventanaAbierta=true){
-                            if(luzEncendida=true){
-                                System.out.print(" [O] ");
-                            }
-                            else{
-                                System.out.print(" [.] ");
+                    System.out.println("Dia " + dias+ " Hora " + horas);
+                    for(losPisos=1; losPisos<=7 ;losPisos++){
+                        for(departamentos=1;departamentos<=5;departamentos++){
+                            if(rayo && departamentos==columnaMuerta){
+                                System.out.println("[R]");
+                            }else{
+                                if((Math.random()*100)<=70){
+                                    if((Math.random()*100)<=60){
+                                        System.out.println("[0]");
+                                    }else{
+                                        System.out.println("[.]");
+                                        }
+                                    }else{
+                                        System.out.println("[#]");
+                                }
                             }
                         }
-                        else{
-                            System.out.print(" [#] ");
-                        }
                     }
-                        
-                    }
-                    System.out.println(""); 
                 }
-                caerRayo=false;
             }
-            dia++;
         }
-        
     }
-}
